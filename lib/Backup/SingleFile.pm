8a515cd969a7428976287ba0e18c90fc770a93cf
Ingo Lantschner <perl@lantschner.name>
2009-08-19 18:07:13 +0000

    Import of LANTI/Backup-SingleFile-0.07 from CPAN.
    gitpan-cpan-distribution: Backup-SingleFile
    gitpan-cpan-version:      0.07
    gitpan-cpan-path:         LANTI/Backup-SingleFile-0.07.tar.gz
    gitpan-cpan-author:       LANTI
    gitpan-cpan-maturity:     released

--- lib/Backup/SingleFile.pm
+++ lib/Backup/SingleFile.pm
@@ -23,11 +23,11 @@ The following documentatoin is still in German - a short description can be foun
 
 =head1 VERSION
 
-Version 0.06
+Version 0.07
 
 =cut
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 
 
 =head1 SYNOPSIS
